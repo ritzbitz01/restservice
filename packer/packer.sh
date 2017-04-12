396f27c79e934160b69502ea357a8d2742782a27
Ritz <ryan.ritz@pearson.com>
2017-04-12 16:30:44 -0600

    Add systemd scripts

--- packer/packer.sh
+++ packer/packer.sh
@@ -1,2 +1,6 @@
 packer build example.json 2>&1 | tee output.txt
 tail -2 output.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ami.txt
+
+
+#With user variables
+packer build -var 'base_ami_id=ami-b9239baf' -var 'ami_app_name=GoCDRestServiceApp' -var 'app_pkg_name=restservice_0.0.5~SNAPSHOT_all.deb' -var 'pkg_install_loc=/home/ubuntu/restservice_0.0.5~SNAPSHOT_all.deb" example.json
